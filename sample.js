293e2ec9732e086596ee1cc4fc883209ce2f20e1
hase75 <122374696+hase75@users.noreply.github.com>
2023-07-21 05:01:29 +0000

    [Finish]js-2

---
+++ sample.js
@@ -0,0 +1,30 @@
+// var str = "webcamp"
+
+// function foo() {
+//   console.log(str)
+//   var y = "hello"
+// }
+
+// foo()
+// console.log(y)
+
+// function foo() {
+//   let x = "webcamp"
+//   {
+//     let y = "hello webcamp"
+//   }
+//   console.log(x)
+//   console.log(y)
+// }
+
+// foo()
+
+var str = "webcamp"
+
+function foo() {
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
+}
+
+foo()
