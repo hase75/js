ca74b0a90d61474c9f24e229c93bedbbb014310d
hase75 <122374696+hase75@users.noreply.github.com>
2023-07-21 02:26:11 +0000

    [Finish]js-2

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-alert('Hello World');
+let hello = 'Hello World';
+alert(hello);
+
+let int1 = 1;
+alert(int1);
+
+alert(4 + 3);
+
+let str1 = 'Hello';
+let str2 = 'World!!';
+alert(str1 + str2);
+
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い')
+}
+
+
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
